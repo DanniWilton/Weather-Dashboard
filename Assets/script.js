9a9fe88fb20c2f95a7398a3ed787c5bdb2d31037
Danni Wilton <danniwilton@gmail.com>
2021-05-01 23:46:43 +0800

    first commit

---
+++ Assets/script.js
@@ -0,0 +1,121 @@
+//API request from current weather API
+
+//saving to local storage 
+
+//display weather 
+
+// const currentWeatherAPI = `http://api.openweathermap.org/data/2.5/box/${city}?bbox=12,32,15,37,10&appid=${apiKey}`
+// const apiKey = "6c06f9f1820d7cce0add7c2c813d7985"
+
+currentDate = moment().format('dddd, MMMM Do YYYY');
+$("#currentDay").text(currentDate);
+
+// fetch weather data
+var weatherData = "";
+
+const searchButton = document.getElementById("searchButton");
+searchButton.addEventListener("click", clickEventFunction)
+
+let search = document.getElementById("searchInput");
+
+function clickEventFunction() {
+
+  let city = search.value
+  var searchedCity = document.getElementById ("searchedCity");
+  searchedCity.textContent = city
+  console.log(city)
+  getFetch(city)
+  }
+
+function getFetch(city) {
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=6c06f9f1820d7cce0add7c2c813d7985`)
+  .then(function (response) {
+    return response.json();})
+  .then(function(data) {
+    console.log(data)
+    weatherData = data
+    var lat = data.coord.lat;
+    var lon = data.coord.lon;
+    console.log(lat, lon)
+    secondFetchCall(lat, lon)
+  ;})
+  .catch (error => console.log(error));}
+
+  function secondFetchCall(lat, lon) {
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=6c06f9f1820d7cce0add7c2c813d7985&units=metric`)
+    .then(function(response) {
+      return response.json();})
+    .then(function(data) {
+      console.log(data)
+      
+      var currentTemp = data.current.temp;
+      var currentTempElement = document.getElementById("currentTemp")
+      currentTempElement.textContent = `Temp: ${currentTemp} °C`
+
+      var currentWind = data.current.wind_speed;
+      var currentWindElement = document.getElementById("currentWind")
+      currentWindElement.textContent = `Wind: ${currentWind}`
+
+      var currentHumidity = data.current.humidity;
+      var currentHumidityElement = document.getElementById("currentHumidity")
+      currentHumidityElement.textContent = `Humidity: ${currentHumidity} `
+
+      var currentUvIndex = data.current.uvi;
+      var currentUvIndexElement = document.getElementById("currentUvIndex")
+      currentUvIndexElement.textContent = `UV Index: ${currentUvIndex}`
+
+      var icon = document.getElementById (`icon`)
+      var image = data.daily[0].weather[0].icon
+      icon.src = `http://openweathermap.org/img/wn/${image}@2x.png`
+
+      for (let i = 1; i < 6; i++) {;
+        var fiveDayForecastTemp = data.daily[i].temp.max
+        var fiveDayForecastWind = data.daily[i].wind_speed
+        var fiveDayForecastHumidity = data.daily[i].humidity
+
+        var tempElement = document.querySelector (`#day${i} .temp`)
+        var windElement = document.querySelector (`#day${i} .wind`)
+        var humidityElement = document.querySelector (`#day${i} .Humidity`)
+        console.log(tempElement)
+        tempElement.textContent = `Temp: ${fiveDayForecastTemp} °C`
+        windElement.textContent = `Wind: ${fiveDayForecastWind}`
+        humidityElement.textContent = `Humidity: ${fiveDayForecastHumidity}`
+
+        var date = document.querySelector (`#day${i} .date `)
+        date.textContent = moment().add(i, 'days').format('DD/MM/YY');
+
+        var icon = document.querySelector (`#day${i} .icon`)
+        var image = data.daily[i].weather[0].icon
+        icon.src = `http://openweathermap.org/img/wn/${image}@2x.png`
+
+      }
+
+      })
+    }
+
+    //retrieve data and append to element
+
+
+
+
+  // icon.src = http://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png
+
+// favourable/moderate/severe
+//icons 
+//local storage
+
+
+// function localStorageSearchHistory() {
+//   cityNames = ["searchedCity"];
+//   localStorage.setItem("searchHistory", JSON.stringify(cityNames));
+// }
+// localStorage.setItem('search', city)
+
+
+
+
+// .fetch(apiInHere).then(function (whatYouWantItToDoWithThisApi){
+//   return whatYouWantItToDoWithThisApi.json()
+
+
+
---
+++ README.md
@@ -0,0 +1,6 @@
+# Weather Dashboard
+
+This simple Weather Dashboard uses an Open Weather API's along with Javascript, with CSS styling.
+When I search a city using search for a city function, I am able to see the current weather for the city searched. The current temperature, humidity, wind speed and UV index is displayed along with the current date and the and an icon displaying what type of weather we are having (i.e. a sun for clear, clouds for cloudy etc.).
+I am also able to see a 5 day forecast below. The date, highest temperature, humidity, wind speed and an icon is displayed. 
+This homework allowed me to be a little bit more creative but I definitely cannot get out of the early 2000 aesthetic. I am understanding the Javascript more and was able to play around with Grid styling more as well. 
